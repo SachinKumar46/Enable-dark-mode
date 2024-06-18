b09f9cc6fe9490153d2ed68ffc4ba9fcefa3b48b
Sachin Rajput <sachinrajput17798@gmail.com>
2024-06-18 16:57:55 +0530

    fixed some changes in navbar class name

--- src/App.js
+++ src/App.js
@@ -32,37 +32,37 @@ function App() {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div classNameName="container-fluid">
-          <a classNameName="navbar-brand" href="#">Navbar</a>
-          <button classNameName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span classNameName="navbar-toggler-icon"></span>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div classNameName="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
-              <li classNameName="nav-item">
-                <a classNameName="nav-link active" aria-current="page" href="#">Home</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li classNameName="nav-item">
-                <a classNameName="nav-link" href="#">Link</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
               </li>
-              <li classNameName="nav-item dropdown">
-                <a classNameName="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </a>
-                <ul classNameName="dropdown-menu">
-                  <li><a classNameName="dropdown-item" href="#">Action</a></li>
-                  <li><a classNameName="dropdown-item" href="#">Another action</a></li>
-                  <li><hr classNameName="dropdown-divider" /></li>
-                  <li><a classNameName="dropdown-item" href="#">Something else here</a></li>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
                 </ul>
               </li>
-              <li classNameName="nav-item">
-                <a classNameName="nav-link disabled" aria-disabled="true">Disabled</a>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
               </li>
             </ul>
-            <form classNameName="d-flex" role="search">
-              <input classNameName="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button classNameName="btn btn-outline-success" type="submit">Search</button>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
           </div>
         </div>
@@ -107,7 +107,7 @@ function App() {
             </div>
           </div>
         </div>
-      <button type='button' classNameName='btn btn-primary btn-sm' onClick={enableDarkModeOnclick}>{btnText}</button>
+      <button type='button' className='btn btn-primary btn-sm' onClick={enableDarkModeOnclick}>{btnText}</button>
       </div>
     </>
   );
